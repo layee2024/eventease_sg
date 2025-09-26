14da3d6c539bf988e8234dbe5478f507a6acd2da
Liang Ang Yee <225235092+layee2024@users.noreply.github.com>
2025-09-26 18:53:39 +0800

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,12 @@
 import './assets/main.css'
 
+// Import Bootstrap CSS & JS
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
 import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+
