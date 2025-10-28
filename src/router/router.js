cee5ef59b0fc5dc3f5001924e5326af99a3bab9d
Liang Ang Yee <225235092+layee2024@users.noreply.github.com>
2025-10-28 12:14:16 +0800

    Fix 2

--- src/router/router.js
+++ src/router/router.js
@@ -14,7 +14,7 @@ import EventsView from '../views/user/events/EventsView.vue'
 import EventDetails from '../views/user/events/EventDetails.vue'
 import ShuffleView from '../views/user/ShuffleView.vue'
 import OnboardingView from '../views/user/Onboarding.vue'
-import ScheduleView from '../views/user/ScheduleView.vue'
+import ScheduleView from '../views/user/scheduleView.vue'
   
 
 
